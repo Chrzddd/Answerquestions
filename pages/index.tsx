3d6b76b5b5d4923b7f307a39d116bcb9f9bf1516
Enzo Gabriel <97063259+Chrzddd@users.noreply.github.com>
2022-06-26 17:50:56 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Questions from '../components/Questions'
 import QuestionModel from '../model/questao'
 import {useRouter} from 'next/router'
 
-const BASE_URL = 'https://answerquestionss-git-main-chrzddd.vercel.app/api'
+const BASE_URL = 'https://answerquestionss-okgt45ocp-chrzddd.vercel.app/api'
 export default function Home() {
   const router = useRouter()
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
